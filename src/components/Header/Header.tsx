06fc49e7882a056143c422776c94cad36e18a4e7
JuliaMatsievich <juliamatsievich@mail.ru>
2024-04-26 17:34:59 +0500

    add: totalcount

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Search } from "../Search/Search";
 import styles from "./Header.module.css";
+import { useAppSelector } from "../../hooks/useAppSelector";
 
 interface IHeadersProps {
   search: string;
@@ -13,9 +14,15 @@ export const Header: FC<IHeadersProps> = ({
   setSearch,
   setIsOpenModalAdd,
 }) => {
+
+  const totalCount = useAppSelector(state => state.pagination.totalCount)
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>Номенклатура</h1>
+      <div className={styles.titleContainer}>
+        <h1 className={styles.title}>Номенклатура</h1>
+        <div className={styles.countItems}>{totalCount} единиц</div>
+      </div>
+
       <div className={styles.buttons}>
         <Search search={search} setSearch={setSearch} />
         <button
